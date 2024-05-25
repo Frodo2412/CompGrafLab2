a263bef4d93f439351973834255441faf1b16ad6
Bruno <brunolemus553@gmail.com>
2024-05-25 10:04:37 -0300

    Initial commit

---
+++ CompGrafLab2/CompGrafLab2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <FreeImage/FreeImage.h>
+
+int main(int argc, char* argv[])
+{
+
+    FreeImage_Initialise();
+    
+    std::cout << "Hello world!\n";
+    return 0;
+}
